65c37724afaee9e3f52b7595ec6de1c6478d036b
phanthinh <quocthinh9889@gmail.com>
2013-11-12 17:38:38 +0700

    add theme

---
+++ themes/frontend/views/layouts/main.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!-- Mirrored from bootswatch.com/journal/ by HTTrack Website Copier/3.x [XR&CO'2013], Tue, 12 Nov 2013 10:19:14 GMT -->
+	<head>
+		<title>Bootswatch: Journal</title>
+		<?php Yii::app()->clientScript->registerCoreScript('jquery');?>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="<?php echo baseTheme();?>/assets/bootstrap.css" media="screen">
+		<link rel="stylesheet" href="<?php echo baseTheme();?>/assets/bower_components/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo baseTheme();?>/assets/assets/css/bootswatch.min.css">
+		<script type="text/javascript">
+
+			
+		</script>
+	</head>
+	<body>
+		
+		<div class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<a href="../index.html" class="navbar-brand">Bootswatch</a>
+					<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="navbar-collapse collapse" id="navbar-main">
+					<ul class="nav navbar-nav">
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Themes <span class="caret"></span></a>
+							<ul class="dropdown-menu" aria-labelledby="themes">
+								<li><a tabindex="-1" href="../default/index.html">Default</a></li>
+								<li class="divider"></li>
+								<li><a tabindex="-1" href="../amelia/index.html">Amelia</a></li>
+								<li><a tabindex="-1" href="../cerulean/index.html">Cerulean</a></li>
+								<li><a tabindex="-1" href="../cosmo/index.html">Cosmo</a></li>
+								<li><a tabindex="-1" href="../cyborg/index.html">Cyborg</a></li>
+								<li><a tabindex="-1" href="../flatly/index.html">Flatly</a></li>
+								<li><a tabindex="-1" href="index.html">Journal</a></li>
+								<li><a tabindex="-1" href="../readable/index.html">Readable</a></li>
+								<li><a tabindex="-1" href="../simplex/index.html">Simplex</a></li>
+								<li><a tabindex="-1" href="../slate/index.html">Slate</a></li>
+								<li><a tabindex="-1" href="../spacelab/index.html">Spacelab</a></li>
+								<li><a tabindex="-1" href="../united/index.html">United</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="../help/index.html">Help</a>
+						</li>
+						<li>
+							<a href="http://news.bootswatch.com/">Blog</a>
+						</li>
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="download">Download <span class="caret"></span></a>
+							<ul class="dropdown-menu" aria-labelledby="download">
+								<li><a tabindex="-1" href="bootstrap.min.css">bootstrap.min.css</a></li>
+								<li><a tabindex="-1" href="bootstrap.css">bootstrap.css</a></li>
+								<li class="divider"></li>
+								<li><a tabindex="-1" href="variables.less">variables.less</a></li>
+								<li><a tabindex="-1" href="bootswatch.less">bootswatch.less</a></li>
+							</ul>
+						</li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="http://builtwithbootstrap.com/" target="_blank">Built With Bootstrap</a></li>
+						<li><a href="https://wrapbootstrap.com/?ref=bsw" target="_blank">WrapBootstrap</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="page-header" id="banner">
+				<div class="row">
+					<div class="col-lg-6">
+						<h1>Journal</h1>
+						<p class="lead">Crisp like a new sheet of paper</p>
+					</div>
+					<div class="col-lg-6">
+						<div class="bsa well">
+							<div id="bsap_1277971" class="bsarocks bsap_c466df00a3cd5ee8568b5c4983b6bb19"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<?php echo $content;?>
+			<footer>
+				<div class="row">
+					<div class="col-lg-12">
+						<ul class="list-unstyled">
+							<li class="pull-right"><a href="#top">Back to top</a></li>
+							<li><a href="http://news.bootswatch.com/" onclick="pageTracker._link(this.href); return false;">Blog</a></li>
+							<li><a href="http://feeds.feedburner.com/bootswatch">RSS</a></li>
+							<li><a href="https://twitter.com/thomashpark">Twitter</a></li>
+							<li><a href="https://github.com/thomaspark/bootswatch/">GitHub</a></li>
+							<li><a href="../help/index.html#api">API</a></li>
+							<li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=F22JEM3Q78JC2">Donate</a></li>
+						</ul>
+						<p>Made by <a href="http://thomaspark.me/">Thomas Park</a>. Contact him at <a href="mailto:hello@thomaspark.me">hello@thomaspark.me</a>.</p>
+						<p>Code licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License v2.0</a>.</p>
+						<p>Based on <a href="http://getbootstrap.com/">Bootstrap</a>. Icons from <a href="http://fortawesome.github.io/Font-Awesome/">Font Awesome</a>. Web fonts from <a href="http://www.google.com/webfonts">Google</a>. Favicon by <a href="https://twitter.com/geraldhiller">Gerald Hiller</a>.</p>
+					</div>
+				</div>
+			</footer>
+		</div>
+		
+		<script src="<?php echo baseTheme();?>/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="<?php echo baseTheme();?>/assets/assets/js/bootswatch.js"></script>
+	</body>
+	<!-- Mirrored from bootswatch.com/journal/ by HTTrack Website Copier/3.x [XR&CO'2013], Tue, 12 Nov 2013 10:19:28 GMT -->
+</html>
